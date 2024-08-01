633b52c723facae8d20033b7ca02dda4e0aaecd1
vidura-kaushalya <vidura.in@gmail.com>
2024-08-01 13:03:30 +0530

    first commit 8

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ const app = express()
 const PORT = 5000
 
 app.use(bodyParser.json())
-app.use(cors())
+const corsOptions = {
+  origin: 'https://66ab34f44430d095b997fcc1--tranquil-torte-9c0ac1.netlify.app',
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
 
 mongoose.connect(
   'mongodb+srv://viduraitexphere:roRLEpN5PnbGKO4q@cluster0.0fmf41p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
