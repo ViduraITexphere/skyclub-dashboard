ab4213b92d036faf3d99748727f337b16496df25
vidura-kaushalya <vidura.in@gmail.com>
2024-08-01 13:39:16 +0530

    first commit 11

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json())
 app.use(
   cors({
     origin: [
-      'https://66ab3e97b9946796316ea2ed--tranquil-torte-9c0ac1.netlify.app/',
+      'https://66ab42537c4d909e836ca634--effortless-longma-ef2d53.netlify.app',
       'https://another-allowed-origin.com',
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
